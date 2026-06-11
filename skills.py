6be68f88449ffb21f95056bfd23962b782c4e195
Keshav Chelmeti <chelmetikeshav@gmail.com>
2026-06-11 11:11:25 +0530

    Add files via upload

---
+++ skills.py
@@ -0,0 +1,16 @@
+SKILLS = [
+    "python",
+    "sql",
+    "excel",
+    "power bi",
+    "tableau",
+    "aws",
+    "machine learning",
+    "deep learning",
+    "pandas",
+    "numpy",
+    "statistics",
+    "data analysis",
+    "etl",
+    "git"
+]
